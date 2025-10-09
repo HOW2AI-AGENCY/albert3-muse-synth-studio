fb4f742a9c1111fe180190dba06df7358901059a
HOW2AI <112258381+ivan-meer@users.noreply.github.com>
2025-10-09 22:51:57 +0700

    feat(tracks): show metadata for track versions

--- src/features/tracks/components/index.ts
+++ src/features/tracks/components/index.ts
@@ -2,3 +2,5 @@ export { TrackCard } from './TrackCard';
 export { TrackListItem } from './TrackListItem';
 export { TrackVersions } from './TrackVersions';
 export { TrackStemsPanel } from './TrackStemsPanel';
+export { TrackVersionMetadataPanel } from './TrackVersionMetadataPanel';
+export type { TrackVersionMetadata } from './TrackVersionMetadataPanel';
